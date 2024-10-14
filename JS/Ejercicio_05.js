4503ce0b1453abda2a6b690a91d0140a21c95722
Edgar Cabrera <230496@utxicotepec.edu.mx>
2024-10-14 11:52:49 -0600

    Practica 7
    Final del ejrcicio 5

--- JS/Ejercicio_05.js
+++ JS/Ejercicio_05.js
@@ -389,6 +389,7 @@ seriesTrending2.forEach((serie, index) => {
 });
 
 
+console.log("%c12.- Filtrado, transformación y busqueda de elementos - (.filter(), .map(), .include()) ", style_console);
 // Usando Filter para filtrar , y map para transformar la información.
 // Lista de series que queremos verificar
 let seriesDeseadas = ["The Walking Dead", "The Boys", "Loki"];
