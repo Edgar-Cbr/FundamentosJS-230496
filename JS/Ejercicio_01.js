e4c992c7a28386d090867b65ff942b5d14ac9f21
Edgar Cabrera Velazquez. <230496@utxicotepec.edu.mx>
2024-10-13 19:21:48 -0600

    Práctica 4
    Se agrego el Ejercicio_02.js y los tipos de datos

--- JS/Ejercicio_01.js
+++ JS/Ejercicio_01.js
@@ -4,19 +4,19 @@
  */
 //Ejercicio 01: Declaración de Variables
 //a) Var
-var miNombre= "Tania";
+var miNombre= "Edgar";
 var misApellidos;
-var miEdad=20;
+var miEdad=19;
 //El objeto de console nos permite enviar datos a la terminal y poder  visualizar el valor de una variable o resultado de una función o método
 console.warn("---Declaración de variables usando: VAR");
 console.log("Intentando leer las variables: ", miNombre, misApellidos);
-misApellidos="Ibarra Salgado";
+misApellidos="Cabrera Velázquez";
 console.log("Ya que ambas fueron inicializadas vuelvo a intentar leerlas: ", miNombre, misApellidos);
 //Una variable puede cambiar de valor en el proceso de ejecución del programa.
 //b) Const
 console.warn("---Declaración de variables del tipo constante usando: CONST")
 const miUniversidad="UT Xicotepec";
-const miMatricula=230369;
+const miMatricula=230496;
 console.log("Hola, ",miNombre, " ", misApellidos, " se que estudias actualmente en: ", miUniversidad," asignada a la matricula: ", miMatricula, " y tienes una edad de: ", miEdad, " años.");
 //Para saber el tipo de datos que tiene una variable o constante podemos utilizar la función type0f()
 console.log("Analizando los datos puedo deducir que: ")
